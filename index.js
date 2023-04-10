b95ef04aa2c68e894cfe0b2b0e9ea0a4930d5db2
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-04-10 17:47:13 -0400

    -m "arrow function_lab_completed"

---
+++ index.js
@@ -0,0 +1,17 @@
+let num1 = 2000
+let num2 = 100
+const divide = (num1, num2) => {
+2000/100
+return 20
+}
+
+let x = 2
+const square = x => {
+x*2
+return 4
+}
+
+let a = 3
+let b = 4
+const add = (a,b) => a+b;
+add(3,4)
